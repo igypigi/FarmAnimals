56b75531212fb1a032dad2cceb9c9ca39555cba0
Igor Lalic <igypigi@gmail.com>
2013-12-02 11:19:48 +0100

    farm :)

---
+++ game/Assets/Scripts/AnimalMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalMove : MonoBehaviour {
+	
+	private static NavMeshAgent agent;
+	public Vector3 moveToPoint;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.Find("Player");
+		Vector3 position = player.transform.position;
+		agent.SetDestination(position);
+	
+	}
+}
---
+++ game/Assets/Scripts/BarnAnimalSelect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarnAnimalSelect : MonoBehaviour {
+	
+	private static string[,] animals =  {{"Pujs", "Pujsa", "Pujsek"}, {"Kurec", "Kura", "Pisancek"}, {"Bik", "Krava", "Tele"}};
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI () {
+		GUI.Box(new Rect(10, 10, 3 * 90 + 20, animals.GetLength(0) * 50 + 50), "Izberi živali");
+		int index = 0;
+		
+		foreach (string animal in animals) {
+			if (GUI.Button(new Rect(index % 3 * 90 + 20, (index / 3 + 1) * 50, 80, 40), animal)) {
+				Debug.Log(animal);
+			}
+			
+			index++;
+		}	
+	}
+}
