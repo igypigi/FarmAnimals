3e1dbbd3b143aba32a6df63333218ee70472f1d6
premik91 <i@premik91.com>
2013-12-07 11:44:04 +0100

    Moved animal select starting to new script.

--- game/Assets/Scripts/AnimalMove.cs
+++ game/Assets/Scripts/AnimalMove.cs
@@ -5,31 +5,43 @@ public class AnimalMove : MonoBehaviour {
 	private static NavMeshAgent agent;
 	private static GameObject player;
 
+	// If animals should move this variable should be set
+	private static Vector3 moveToPoint;
+	private static bool moving;
+
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
 		player = GameObject.Find ("Player");
-		moveToPoint = GameObject.Find ("AnimalBarnSpot").transform.position;
 	}
-
-	// If animals should move this variable should be set
-	private static Vector3 moveToPoint;
 
 	// Update is called once per frame
 	void Update () {
-		float distance = Vector3.Distance(gameObject.transform.position, moveToPoint);
-		if (distance > 0.2) {
-			// If distance is larger than 10.0 than run else walk
-			if (distance > 10.0) {
-				agent.speed = 7;
-				animation.Play("run", PlayMode.StopAll);
+		if (moveToPoint != null) {
+			// Distance beetwen move to point and animal
+			float distance = Vector3.Distance(gameObject.transform.position, moveToPoint);
+			if (distance > 0.2 && moving) {
+				// If distance is larger than 10.0 than run else walk
+				if (distance > 10.0) {
+					agent.speed = 7;
+					animation.Play("run", PlayMode.StopAll);
+				} else {
+					agent.speed = 4;
+					animation.Play("walk", PlayMode.StopAll);
+				}
+				agent.SetDestination(moveToPoint);
 			} else {
-				agent.speed = 4;
-				animation.Play("walk", PlayMode.StopAll);
+				// Animal reached spot, so stop movement
+				moveToPoint = null;
+				moving = false;
 			}
-			agent.SetDestination(moveToPoint);
 		} else {
 			animation.Play("idle", PlayMode.StopAll);
 		}
+	}
+
+	public static void setMoving (bool move, Vector3 moveTo) {
+		moving = move;
+		moveToPoint = moveTo;
 	}
 }
---
+++ game/Assets/Scripts/StartBarnSelect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartBarnSelect : MonoBehaviour
+{
+	private static bool userOnSpot = false;
+	private static GameObject player;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.Return)) {
+			userOnSpot = !userOnSpot;
+		}
+
+		// If user presses Return and is standing on a pod than start animal select
+		float distance = Vector3.Distance (gameObject.transform.position, player.transform.position);
+		if (distance < 1.9 && userOnSpot) {
+			Debug.Log ("User on spot");
+		}
+	}
+}
