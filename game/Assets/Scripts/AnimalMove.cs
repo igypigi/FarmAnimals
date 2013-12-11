15407f443fc6f26e6b7e124a51cc8865dce33cf2
premik91 <i@premik91.com>
2013-12-11 22:46:11 +0100

    Some restructuring.

--- game/Assets/Scripts/AnimalMove.cs
+++ game/Assets/Scripts/AnimalMove.cs
@@ -8,7 +8,9 @@ public class AnimalMove : MonoBehaviour {
 
 	// If animals should move this variable should be set
 	private Vector3 moveToPoint;
-	private bool moving;
+	private bool moveNavmesh;
+	private bool moveRandom = false;
+	private bool moveInProgress = false;
 
 	// Use this for initialization
 	void Start () {
@@ -20,30 +22,17 @@ public class AnimalMove : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (moving) {
-			// Distance beetwen move to point and animal
-			float distance = Vector3.Distance(gameObject.transform.position, moveToPoint);
-			if (distance > 0.2) {
-				// If distance is larger than 10.0 than run else walk
-				if (distance > 10.0) {
-					agent.speed = 7;
-					animation.Play("Run", PlayMode.StopAll);
-				} else {
-					agent.speed = 4;
-					animation.Play("Walk", PlayMode.StopAll);
-				}
-				agent.SetDestination(moveToPoint);
-			} else {
-				// Animal reached spot, so stop movement
-				moving = false;
-			}
-		} else {
+		if (moveNavmesh) {
+			navmeshMove ();
+		} else if (moveRandom) {
+			randomMove ();
+		} else if (!moveInProgress) {
 			animation.Play("Idle", PlayMode.StopAll);
 		}
 	}
 
 	public void setMoving (Vector3 moveTo) {
-		moving = true;
+		moveNavmesh = true;
 		moveToPoint = moveTo;
 	}
 
@@ -53,5 +42,30 @@ public class AnimalMove : MonoBehaviour {
 
 	public void goToFence () {
 		setMoving (GameObject.Find(animal.Breed + "Fence").transform.position);
+		// Start random move
+		moveRandom = true;
+	}
+
+	public void randomMove () {
+
+	}
+
+	public void navmeshMove () {
+		// Distance beetwen move to point and animal
+		float distance = Vector3.Distance(gameObject.transform.position, moveToPoint);
+		if (distance > 0.2) {
+			// If distance is larger than 10.0 than run else walk
+			if (distance > 10.0) {
+				agent.speed = 7;
+				animation.Play("Run", PlayMode.StopAll);
+			} else {
+				agent.speed = 4;
+				animation.Play("Walk", PlayMode.StopAll);
+			}
+		} else {
+			// Animal reached spot, so stop movement
+			moveNavmesh = false;
+		}
+		agent.SetDestination(moveToPoint);
 	}
 }
