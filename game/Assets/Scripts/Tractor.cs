f3046aab74e6f8110f58d2386a10308a96ab65d7
Igor Lalic <igypigi@gmail.com>
2014-01-10 10:46:11 +0100

    added drivable tractor

---
+++ game/Assets/Scripts/Tractor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tractor : MonoBehaviour {
+
+	private const float maxDistanceToTractor = 5.0f;
+
+	private bool drivingTractor = false;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			if (drivingTractor) {
+				drivingTractor = false;
+				player.SetActive(true);
+				this.GetComponent<ThirdPersonController>().enabled = false;
+				this.GetComponent<ThirdPersonCamera>().enabled = false;
+				Vector3 pos = transform.position;
+				pos.x += 1.5f;
+				pos.y += 1.0f;
+				player.transform.position = pos;
+			} else {
+				Vector3 playerPosition = player.transform.position;
+				float dist = Vector3.Distance(playerPosition, transform.position);
+				if (dist < maxDistanceToTractor) {
+					drivingTractor = true;
+					player.SetActive(false);
+					this.GetComponent<ThirdPersonController>().enabled = true;
+					this.GetComponent<ThirdPersonCamera>().enabled = true;
+				}
+			}
+		}
+	}
+}
